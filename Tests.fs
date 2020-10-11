13b4c855196de5b2f337d6e4da95969143439942
Serhii <sergiybiluk@gmail.com>
2020-10-11 16:12:32 +0200

    add more tests

--- Tests.fs
+++ Tests.fs
@@ -6,11 +6,18 @@ open Utils.Board
 open Types.GameTypes
 
 [<Fact>]
-let First () =
+let ``Move point by (1,1)`` () =
     let result = movePoint (Point(2, 2)) (1, 1)
     Assert.Equal(result, Point(3, 3))
 
 [<Fact>]
-let Second () =
-    let result = movePoint (Point(2, 2)) (2, 2)
-    Assert.Equal(result, Point(4, 3))
+let ``Don't move the point`` () =
+    let point = Point(2, 2)
+    let result = movePoint point (0, 0)
+    Assert.Equal(result, point)
+
+[<Fact>]
+let ``Move point in specified direction`` () =
+    let point = Point(2, 2)
+    let result = movePointByIndex N point 1
+    Assert.Equal(result, Point(1, 2))
